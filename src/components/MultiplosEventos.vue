7903be0d1f2e415bd468105b6fabdd22ee0fea06
lealalves <leealalves@hotmail.com>
2022-01-23 12:09:06 -0300

    Múltiplos eventos

---
+++ src/components/MultiplosEventos.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <button @click="primeiro(txt1), segundo(txt2), terceiro()">Ativar múltiplos eventos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MultiplosEventos',
+    data(){
+        return {
+            multiplos_eventos: "",
+            txt1: 'Primeiro evento',
+            txt2: 'Segundo evento'
+        }
+    },
+    methods: {
+        primeiro(txt) {
+            this.multiplos_eventos = txt
+        },
+        segundo(txt){
+            setTimeout(() => this.multiplos_eventos = txt, 1000);
+        },
+        terceiro() {
+            setTimeout(() => this.multiplos_eventos = "", 2000);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div{
+        margin: 100px
+    }
+</style>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -2,12 +2,19 @@
     <div>
         <h1>Olá Vue!</h1>
         <h2>Meu nome é {{ name }} e atualmente sou {{ profissao }}.</h2>
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+import MultiplosEventos from "./MultiplosEventos.vue"
+
+
 export default {
     name: 'PrimeiroComponente',
+    components: {
+        MultiplosEventos
+    },
     data(){
         return {
             name: 'Leal',
